642715a5edd632407b0f257aa1836178f03ce3fe
Sebastian Wehrmann <sebastian@wehrmann.de>
2021-01-27 11:29:22 +0100

    Add logging

--- clients/baseclient.py
+++ clients/baseclient.py
@@ -1,6 +1,10 @@
 import asyncio
 import json
 
+import logging
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
 
 class BaseClient:
 
@@ -37,13 +41,22 @@ class BaseClient:
                 self.history = self.history[0-self.keep_items:]
                 result = self.data
             except Exception as e:
-                print(f'Exception while fetching {self._type} data: {e}')
+                log.error(f'Exception while fetching {self.type_} data: {e}')
             else:
                 if result:
                     result['DeviceClass'] = self.type_
                     if result not in self.history:
                         self.history.append(result)
-                    for websocket in list(self.websockets):
-                        await self.send(websocket, result)
+                        log.info(
+                            f'{self.type_} fetched new data, sending to '
+                            f'{len(self.websockets)} clients.'
+                        )
+                        for websocket in list(self.websockets):
+                            await self.send(websocket, result)
+                    else:
+                        log.info(
+                            f'{self.type_} fetched existing data, not sending '
+                            f'to {len(self.websockets)} clients.'
+                        )
             finally:
                 await asyncio.sleep(self.sleep_time)
