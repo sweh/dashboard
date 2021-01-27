eb230e5fe50a2f5e0359753222ffa3a258c24e2b
Sebastian Wehrmann <sebastian@wehrmann.de>
2021-01-27 14:38:12 +0100

    Save current pv data to temorary file for munin plugin

--- clients/pv.py
+++ clients/pv.py
@@ -1,3 +1,4 @@
+import json
 from speedwiredecoder import decode_speedwire
 from clients.baseclient import BaseClient
 
@@ -7,11 +8,26 @@ class Client(BaseClient):
     sleep_time = 1
     type_ = 'PV'
     keep_items = 100
+    data_file = '/tmp/pvdata.txt'
 
     def __init__(self, smadaemon):
         self.smadaemon = smadaemon
         super(Client, self).__init__(smadaemon.config)
         self.sock = smadaemon.connect_to_socket()
+
+    def save_result_to_file(self, data):
+        data = dict(
+            panelpower=data['AC Power Solar'],
+            batterypower=0-data['AC Power Battery'],
+            power_from_grid=data['Power from grid'],
+            power_to_grid=data['Power to grid']
+        )
+        data['consumption'] = (
+                data['panelpower'] + data['batterypower'] +
+                data['power_from_grid'] - data['power_to_grid']
+        )
+        with open(self.data_file, 'w') as f:
+            f.write(json.dumps(data))
 
     @property
     def data(self):
@@ -27,6 +43,8 @@ class Client(BaseClient):
                         if item['DeviceClass'] == 'Battery Inverter':
                             item['AC Power Battery'] = item['AC Power']
                         result.update(item)
+        if result:
+            self.save_result_to_file(result)
         return result
 
     def run_features(self, emparts):
