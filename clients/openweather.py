c4b89eaecad14e06297d4d836964830ef5a85821
Sebastian Wehrmann <sebastian@wehrmann.de>
2021-02-15 11:37:21 +0100

    Bugfixes

--- clients/openweather.py
+++ clients/openweather.py
@@ -39,30 +39,30 @@ class Client(BaseClient):
         station = weatherData.stationByName('Am Wachtelberg 14 (Wohnzimmer)')
         for module in station['modules']:
             if module['type'] == 'NAModule1':
-                result['out_temp_battery'] = module['battery_percent']
+                # result['out_temp_battery'] = module['battery_percent']
                 result['out_temp'] = module['dashboard_data']['Temperature']
-                result['out_temp_min'] = module['dashboard_data']['min_temp']
-                result['out_temp_max'] = module['dashboard_data']['max_temp']
-                result['out_humi'] = module['dashboard_data']['Humidity']
-                temp_trend = module['dashboard_data']['temp_trend']
-                temp_trend = (
-                    'right' if temp_trend == 'stable' else temp_trend
-                )
-                result['out_temp_trend'] = temp_trend
-            elif module['type'] == 'NAModule3':
-                result['rain_battery'] = module['battery_percent']
-                result['rain'] = module['dashboard_data']['Rain']
-                result['rain_1'] = module['dashboard_data']['sum_rain_1']
-                result['rain_24'] = module['dashboard_data']['sum_rain_24']
-            elif module['type'] == 'NAModule2':
-                result['wind_battery'] = module['battery_percent']
-                result['wind_str'] = module['dashboard_data']['WindStrength']
-                result['wind_angle'] = module['dashboard_data']['WindAngle']
-            elif module['type'] == 'NAModule4':
-                result['temp_in_battery'] = module['battery_percent']
-                result['in_temp'] = module['dashboard_data']['Temperature']
-                result['in_humi'] = module['dashboard_data']['Humidity']
-                result['in_co2'] = module['dashboard_data']['CO2']
+                # result['out_temp_min'] = module['dashboard_data']['min_temp']
+                # result['out_temp_max'] = module['dashboard_data']['max_temp']
+                # result['out_humi'] = module['dashboard_data']['Humidity']
+                # temp_trend = module['dashboard_data']['temp_trend']
+                # temp_trend = (
+                #     'right' if temp_trend == 'stable' else temp_trend
+                # )
+                # result['out_temp_trend'] = temp_trend
+            # elif module['type'] == 'NAModule3':
+            #     result['rain_battery'] = module['battery_percent']
+            #     result['rain'] = module['dashboard_data']['Rain']
+            #     result['rain_1'] = module['dashboard_data']['sum_rain_1']
+            #     result['rain_24'] = module['dashboard_data']['sum_rain_24']
+            # elif module['type'] == 'NAModule2':
+            #     result['wind_battery'] = module['battery_percent']
+            #     result['wind_str'] = module['dashboard_data']['WindStrength']
+            #     result['wind_angle'] = module['dashboard_data']['WindAngle']
+            # elif module['type'] == 'NAModule4':
+            #     result['temp_in_battery'] = module['battery_percent']
+            #     result['in_temp'] = module['dashboard_data']['Temperature']
+            #     result['in_humi'] = module['dashboard_data']['Humidity']
+            #     result['in_co2'] = module['dashboard_data']['CO2']
         return result
 
     @property
