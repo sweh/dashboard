b333e522ffbc95094f397f1c6aa2b25c2f39eedb
Sebastian Wehrmann <sebastian@wehrmann.de>
2021-02-24 16:30:21 +0100

    Use battery charge instead of discharge

--- clients/pv.py
+++ clients/pv.py
@@ -52,15 +52,12 @@ class Client(BaseClient):
             'Consumption',
             'AC Power Solar',
             'Power to grid',
+            'AC Power Battery',
             'Power from grid'
         ):
             sums.setdefault(key, 0)
-            sums[key] += result[key] / 3600 * seconds
-        sums.setdefault('AC Power Battery', 0)
-        if result['AC Power Battery'] < 0:
-            sums['AC Power Battery'] += (
-                (0 - result['AC Power Battery']) / 3600 * seconds
-            )
+            if result[key] > 0:
+                sums[key] += result[key] / 3600 * seconds
         result['sums'] = sums
 
     def calculate_costs(self, result):
