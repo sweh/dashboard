8f5b6e07fbff99e03d4520877033d2a0d5d8d0c2
Sebastian Wehrmann <sebastian@wehrmann.de>
2021-03-29 08:10:50 +0200

    Update corona Stand display

--- clients/corona.py
+++ clients/corona.py
@@ -1,5 +1,6 @@
 import requests
 from clients.baseclient import BaseClient
+from datetime import date, timedelta
 
 
 class Client(BaseClient):
@@ -14,7 +15,12 @@ class Client(BaseClient):
     @property
     def data(self):
         result = requests.get(self.corona_url).text
-        stand = result.split('(Stand ')[1][:21]
+        stand = result.split('(Stand ')[1][:10]
+        today = date.today()
+        if stand == today.strftime('%d.%m.%Y'):
+            stand = 'heute'
+        if stand == (today - timedelta(days=1)).strftime('%d.%m.%Y'):
+            stand = 'gestern'
         gesamt = (
             result
             .split('Im Landkreis Wittenberg wurden ')[1]
