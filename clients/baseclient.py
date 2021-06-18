830eed5d83c1c744793d49d565e4f43d0938ec98
Sebastian Wehrmann <sebastian@wehrmann.de>
2021-06-18 12:46:22 +0200

    Save data to influxdb

--- clients/baseclient.py
+++ clients/baseclient.py
@@ -1,7 +1,10 @@
 import asyncio
+from influxdb import InfluxDBClient
+from influxdb.client import InfluxDBClientError
 from datetime import datetime
 from history import History
 import json
+import time
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -67,6 +70,7 @@ class BaseClient:
     async def prepare_send(self, result):
         if result not in self.history:
             self.history.append(result)
+            self.save_to_influx(result)
             log.info(
                 f'{self.type_} fetched new data, sending to '
                 f'{len(self.websockets)} clients.'
@@ -77,6 +81,76 @@ class BaseClient:
             log.info(
                 f'{self.type_} fetched existing data, not sending '
                 f'to {len(self.websockets)} clients.'
+            )
+
+    def save_to_influx(self, result):
+        if not bool(int(self.config.get('FEATURE-influxdb', 'enabled'))):
+            return
+        db = self.config.get('FEATURE-influxdb', 'db')
+        host = self.config.get('FEATURE-influxdb', 'host')
+        port = int(self.config.get('FEATURE-influxdb', 'port'))
+        ssl = False
+        timeout = int(self.config.get('FEATURE-influxdb', 'timeout'))
+        user = self.config.get('FEATURE-influxdb', 'user')
+        password = self.config.get('FEATURE-influxdb', 'password')
+
+        try:
+            if ssl is True:
+                influx = InfluxDBClient(
+                    host=host, port=port, ssl=ssl, verify_ssl=ssl,
+                    username=user, password=password, timeout=timeout
+                )
+            else:
+                influx = InfluxDBClient(
+                    host=host, port=port, username=user, password=password,
+                    timeout=timeout
+                )
+
+            dbs = influx.get_list_database()
+            if not {"name": db} in dbs:
+                influx.create_database(db)
+
+            influx.switch_database(db)
+
+        except InfluxDBClientError as e:
+            log.error(
+                "InfluxDB:  Connect Error to '%s' @ '%s'(%s)" % (
+                    str(user), host, db)
+            )
+            log.error(format(e))
+            return
+        except Exception as e:
+            log.error(
+                "InfluxDB: Error while connecting to '%s' @ '%s'(%s)" % (
+                    str(user), host, db)
+            )
+            log.error(e)
+            return
+
+        influx_data = {}
+        influx_data['measurement'] = self.type_
+        influx_data['time'] = datetime.datetime.now()
+        influx_data['tags'] = {}
+        influx_data['fields'] = result
+
+        points = [influx_data]
+        try:
+            influx.write_points(points, time_precision='s', protocol='json')
+        except InfluxDBClientError as e:
+            log.error('InfluxDBError: %s' % (format(e)))
+            log.error(
+                "InfluxDB failed data:" +
+                format(time.strftime("%H:%M:%S", time.localtime(
+                    influx_data['time']))
+                ),
+                format(points)
+            )
+        else:
+            log.info("InfluxDB: new data published %s:%s" % (
+                format(time.strftime(
+                    "%H:%M:%S", time.localtime(influx_data['time']))
+                ),
+                format(points))
             )
 
     async def run(self, once=False):
